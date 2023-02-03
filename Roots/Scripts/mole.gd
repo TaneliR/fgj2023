71999b11077a200c88417bd870130f46770cba91
Anssi <anssi.hietaniemi@gmail.com>
2023-02-04 00:04:31 +0200

    Add kineticbody and script to mole.tscn

---
+++ Roots/Scripts/mole.gd
@@ -0,0 +1,20 @@
+extends KinematicBody2D
+
+export (int) var speed = 200
+
+onready var target = position
+var velocity = Vector2()
+
+func _ready():
+    add_to_group("moles")
+
+func _input(event):
+    if event.is_action_pressed("click"):
+        target = get_global_mouse_position()
+        print(target)
+
+func _physics_process(delta):
+    velocity = position.direction_to(target) * speed
+    look_at(target)
+    if position.distance_to(target) > 5:
+        velocity = move_and_slide(velocity)
