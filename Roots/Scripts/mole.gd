5b5b2ca9da957703f178b275121a035abce3df8f
Anssi <anssi.hietaniemi@gmail.com>
2023-02-04 15:06:02 +0200

    Add clickable function to mole

--- Roots/Scripts/mole.gd
+++ Roots/Scripts/mole.gd
@@ -8,13 +8,23 @@ var velocity = Vector2()
 func _ready():
 	add_to_group("moles")
 
-func _input(event):
+func _input_event(viewport, event, shape_idx):
 	if event.is_action_pressed("click"):
+		print("osu")
+
+func _input(event):
+	if event.is_action_pressed("ui_up"):
 		target = get_global_mouse_position()
 		print(target)
+		print(position.direction_to(target).x)
+		if position.direction_to(target).x < 0:
+			get_node("AnimatedSprite").set_flip_v(true)
+		else:
+			get_node("AnimatedSprite").set_flip_v(false)
 
 func _physics_process(delta):
 	velocity = position.direction_to(target) * speed
 	look_at(target)
 	if position.distance_to(target) > 5:
 		velocity = move_and_slide(velocity)
+	
