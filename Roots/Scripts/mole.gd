b0519466b593aef7796f6daa6ca1338df8967853
Anssi <anssi.hietaniemi@gmail.com>
2023-02-04 15:55:29 +0200

    Add click-to-kill to mole

--- Roots/Scripts/mole.gd
+++ Roots/Scripts/mole.gd
@@ -1,6 +1,7 @@
 extends KinematicBody2D
 
 export (int) var speed = 200
+var hits = 10
 
 onready var target = position
 var velocity = Vector2()
@@ -8,9 +9,16 @@ var velocity = Vector2()
 func _ready():
 	add_to_group("moles")
 
+func take_damage():
+	hits -= 1
+	print(hits)
+	if hits < 1:
+		queue_free()
+
 func _input_event(viewport, event, shape_idx):
 	if event.is_action_pressed("click"):
 		print("osu")
+		take_damage()
 
 func _input(event):
 	if event.is_action_pressed("ui_up"):
