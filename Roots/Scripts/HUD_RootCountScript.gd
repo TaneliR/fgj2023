8a05ea7bbb7573374cb0ba8924c531b17a51d47d
paavonelimarkka <paavo.nelimarkka@gmail.com>
2023-02-05 12:59:11 +0200

    fix rootcount

--- Roots/Scripts/HUD_RootCountScript.gd
+++ Roots/Scripts/HUD_RootCountScript.gd
@@ -1,11 +1,11 @@
 extends Label
 
 var roots = 0
-
 export var max_roots = 500
 
 func _ready():
+	print (roots)
 	text = str(roots) + " / " + str(max_roots)
 	
-# on signal x
-#	text = roots +1
+func _process(delta):
+	text = str(get_tree().get_nodes_in_group("roots").size())
